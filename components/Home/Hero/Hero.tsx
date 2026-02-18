a62ef9a59e554df1e677280ce0837094a7aaa208
arjunsinghx2004 <as9056611@gmail.com>
2026-02-18 14:47:54 +0530

    fix

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
           height={150}
           className="rounded-full border-8 border-[#0c0c48aa]"
         />
-        <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide">
           creating web products,
           <br />
           brands,
